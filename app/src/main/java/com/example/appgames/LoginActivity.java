8b5f9f9123da0221b49f6187b8500c55390e5872
guizinhosilvasantos10@gmail.com <guizinhosilvasantos10@gmail.com>
2023-03-31 01:06:19 -0300

    apareceu, mas com parte branca amostra

--- app/src/main/java/com/example/appgames/LoginActivity.java
+++ app/src/main/java/com/example/appgames/LoginActivity.java
@@ -1,14 +1,44 @@
 package com.example.appgames;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
 
+public class LoginActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+
+        bottomNavigationView.setSelectedItemId(R.id.bottom_navigation);
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+                when(item.itemId) {
+            R.id.item1 -> {
+                // Respond to navigation item 1 click
+                true
+            }
+            R.id.item2 -> {
+                // Respond to navigation item 2 click
+                true
+            }
+        else -> false
+        }
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
